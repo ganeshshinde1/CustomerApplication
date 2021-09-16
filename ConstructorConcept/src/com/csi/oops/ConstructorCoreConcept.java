a6de09779437662dcf3bda7244c93fb7f7a692c8
ganeshshinde1 <ganeshshinde2681998@gmail.com>
2021-09-16 02:17:49 -0700

    initial commit

---
+++ ConstructorConcept/src/com/csi/oops/ConstructorCoreConcept.java
@@ -0,0 +1,27 @@
+package com.csi.oops;
+class Employee{
+	int empId = 121;
+	String empName = "SWARA";
+	double empSalary = 35000.50;
+	String empAddress;
+	long empContactNumber;
+	public Employee() {
+		// TODO Auto-generated constructor stub
+		System.out.println(" \n Employee Id:" + empId + " \n Employee Name: " + empName + " \n Employee salary:"+ empSalary);
+	}
+	public Employee ( String empAddress,long empContactNumber, String  empEmailId){
+		System.out.println("\n Employee Address:" + empAddress + "\n Employee Contact number:"+ empContactNumber + "\n Employee Email Id: " + empEmailId);
+		this.empAddress = empAddress;
+		this.empContactNumber = empContactNumber;
+	}
+	public void displayData (){
+		System.out.println("\n Employee Address:" + empAddress + "\n EmployeeContactNumber:"+ empContactNumber) ;
+	}
+}
+public class ConstructorCoreConcept {
+public static void main(String[] args) {
+Employee e1 = new Employee ();
+Employee e2 = new Employee ("PUNE" , 56254524545L,   "BINU@CSI.COM");
+e2.displayData();
+}
+}
